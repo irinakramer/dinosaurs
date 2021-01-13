0d25a7b54a30523be8a437d67c9f8b21c566a19b
Irina Kramer <irakramer@gmail.com>
2021-01-13 13:44:53 -0800

    feat: Fetch json data

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
+// Fetch object data from JSON
 
-    // Create Dino Constructor
-
-
-    // Create Dino Objects
-
-
-    // Create Human Object
-
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+let dinos = [];
+function fetchDinoData() {
+    fetch('./dino.json')
+        .then(response => response.json())
+        .then(data => {
+            dinos = data;
+            console.log(dinos);
+            return dinos;
+        })
+        .catch(error => console.log(error))
+        .finally(() => { });
+}
+fetchDinoData();
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
 
-    // Remove form from screen
+// Create Dino Constructor
+
+
+// Create Dino Objects
+
+
+// Create Human Object
+
+// Use IIFE to get human data from form
+
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches. 
+
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
